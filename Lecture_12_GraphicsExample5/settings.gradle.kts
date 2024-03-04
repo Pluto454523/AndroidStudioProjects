97f7fbba59def00e919dd595c77f45da1dfc0603
PLUTO <nawin3399@gmail.com>
2024-03-04 12:05:40 +0700

    add lecture 12 exmaple (1-8)

---
+++ Lecture_12_GraphicsExample5/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lecture_12_GraphicsExample5"
+include(":app")
+ 
